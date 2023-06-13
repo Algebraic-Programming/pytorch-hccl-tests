d20cd80602488183236179be334a7c9cc9da6125
Anastasios Zouzias <zouzias@users.noreply.github.com>
2023-06-13 09:45:04 +0200

    [hotfix] fix multi_lat bench; improve make (#15)
    Co-authored-by: Anastasios Zouzias <anastasios.zouzias@huawei.com>

--- pytorch_hccl_tests/osu/p2p/osu_multi_lat.py
+++ pytorch_hccl_tests/osu/p2p/osu_multi_lat.py
@@ -3,6 +3,7 @@ import os
 import sys
 from time import perf_counter as now
 
+import pandas as pd
 import torch
 import torch.distributed as dist
 
@@ -21,16 +22,17 @@ logger = logging.getLogger(__name__)
 
 def osu_multi_lat(args):
     rank = dist.get_rank()
-    numprocs = dist.get_world_size()
-    pairs = int(numprocs / 2)
+    world_size = dist.get_world_size()
+    pairs = int(world_size / 2)
     dtype = torch.float32
     device = get_device(rank)
     pg = None
 
     options = Options("Multi Latency", args)
-    Utils.check_numprocs(numprocs, rank, limit=2)
 
     Utils.print_header(options.benchmark, rank)
+
+    df = pd.DataFrame(columns=["size_in_bytes", "avg_latency"])
 
     for size in Utils.message_sizes(options):
         if size > options.large_message_size:
@@ -59,7 +61,17 @@ def osu_multi_lat(args):
                 dist.send(s_msg, partner, pg, 1)
             toc = now()
 
-        Utils.print_stats(toc, tic, 2 * options.iterations, rank, numprocs, size)
+        avg_latency = Utils.avg_lat(toc, tic, 2 * options.iterations, world_size)
+
+        if rank == 0:
+            logger.info("%-10d%18.2f" % (size, avg_latency))
+            df = df.append(
+                {"size_in_bytes": int(size), "avg_latency": avg_latency.item()},
+                ignore_index=True,
+            )
+
+        # Persist result to CSV file
+        df.to_csv(f"osu_multi_latency-{world_size}.csv", index=False)
 
 
 def main():
