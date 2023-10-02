cfae4fa67ae110f47cc49760e1a5421d2ee4ec25
Anastasios Zouzias <anastasios.zouzias@huawei.com>
2023-10-02 10:13:09 +0200

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/Algebraic-Programming/pytorch-hccl-tests",
-    version="0.1.8",
+    version="0.1.9",
     zip_safe=False,
 )
